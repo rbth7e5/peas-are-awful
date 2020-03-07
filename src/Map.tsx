29df683052b1a9240f3fc6aa07ee87299f848b22
rbth7e5 <caijie96@gmail.com>
2020-03-07 12:35:46 -0500

    Add stylesheet

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View} from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import {STYLES} from './styles';
 
 export default function Map() {
-  return <View />;
+  return <MapboxGL.MapView style={STYLES.mapView} />;
 }
---
+++ src/styles.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export const STYLES = StyleSheet.create({
+  mapView: {
+    height: '100%',
+    width: '100%',
+  },
+});
