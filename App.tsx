ca65b35bd78bbab2a1e93fbb98d7b79b8535aa51
rbth7e5 <caijie96@gmail.com>
2020-03-07 12:05:27 -0500

    Add icons

--- App.tsx
+++ App.tsx
@@ -13,13 +13,34 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Map from './src/Map';
 import Profile from './src/Profile';
 import Community from './src/Community';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Map') {
+              iconName = focused ? 'map' : 'map-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'account-circle' : 'account-circle-outline';
+            } else if (route.name === 'Community') {
+              iconName = focused ? 'account-group' : 'account-group-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
         <Tab.Screen name="Map" component={Map} />
         <Tab.Screen name="Profile" component={Profile} />
         <Tab.Screen name="Community" component={Community} />
