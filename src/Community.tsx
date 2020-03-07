cf24904f3efccc843f5e1ec35e891e056d2990ee
Winni Lee <winnilee@10-18-92-208.dynapool.wireless.nyu.edu>
2020-03-07 13:35:06 -0500

    Init community

--- src/Community.tsx
+++ src/Community.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {SearchBar} from 'react-native-elements';
 
 export default function Community() {
-  return <View />;
+  return (
+    <>
+      <StatusBar />
+      <SafeAreaView>
+        <SearchBar placeholder="Search" />
+      </SafeAreaView>
+    </>
+  );
 }
