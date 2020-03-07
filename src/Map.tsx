e6bf70f0d7136fbdbcaf8ee9cffaf42eadf0ca4b
rbth7e5 <caijie96@gmail.com>
2020-03-07 13:22:58 -0500

    Get map to show

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import {STYLES} from './styles';
+import {MAPBOX_TOKEN} from '../private';
+
+MapboxGL.setAccessToken(MAPBOX_TOKEN);
 
 export default function Map() {
-  return <MapboxGL.MapView style={STYLES.mapView} />;
+  return (
+    <MapboxGL.MapView style={STYLES.mapView}>
+      <MapboxGL.Camera followUserLocation={true} />
+      <MapboxGL.UserLocation visible={true} />
+    </MapboxGL.MapView>
+  );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -2,6 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export const STYLES = StyleSheet.create({
   mapView: {
+    flex: 1,
     height: '100%',
     width: '100%',
   },
